4eb4674e11f0200f8e9cb75f9e6394b0cf99fa3e
Karim.2501 <karimmitra0@gmail.com>
2022-07-07 14:02:55 +0700

    Listview(07 Juli 2022, 02:02 PM)

--- app/src/main/java/com/example/scancoordinate/MainActivity.kt
+++ app/src/main/java/com/example/scancoordinate/MainActivity.kt
@@ -2,33 +2,42 @@ package com.example.scancoordinate
 
 import android.content.ClipData
 import android.content.ClipboardManager
-import android.content.ContentValues.TAG
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.TextView
 import android.widget.Toast
-import androidx.core.view.indices
+import androidx.appcompat.app.AlertDialog
 import com.example.scancoordinate.databinding.ActivityMainBinding
+import com.example.scancoordinate.databinding.InputDialogBinding
 import io.realm.Realm
 import io.realm.RealmConfiguration
+import io.realm.RealmResults
+import io.realm.exceptions.RealmException
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var binding2: InputDialogBinding
+    lateinit var realm: Realm
+//    lateinit var Models: ArrayList<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initRealm()
+
+//        val helper = Helper()
+//        Models = helper.retrieve()!!
+//
+//        binding.lvKoordinat.adapter = listAdapter(this, R.layout.list_item, Models)
 
         //Listview
-        var dataNamaKoordinat = arrayOf("Roma", "Bali", "Toraja", "Rusia", "Arab Saudi","Bengkulu", "Amerika Serikat", "Danau Toba", "China", "Turki")
-        var dataKoordinat = arrayOf(
+        val dataNamaKoordinat = arrayOf("Roma", "Bali", "Toraja", "Rusia", "Arab Saudi","Bengkulu", "Amerika Serikat", "Danau Toba", "China", "Turki")
+        val dataKoordinat = arrayOf(
             "-3.8958674397239164, 102.27939432259899",
             "-0.9139841225013886, 100.34937045954474",
             "2.7875607432569285, 98.74984595792846",
@@ -44,22 +53,56 @@ class MainActivity : AppCompatActivity() {
         for (i in dataKoordinat.indices){
             daftar.add(list(dataNamaKoordinat[i], dataKoordinat[i]))
         }
-        binding.listview.adapter = listAdapter(this, R.layout.list_item, daftar)
-        binding.listview.isClickable = true
-        binding.listview.setOnItemClickListener { parent, view, position, id ->
-            binding.ETinputNama.setText(dataNamaKoordinat[position],TextView.BufferType.EDITABLE)
-            binding.ETinputCo.setText(dataKoordinat[position],TextView.BufferType.EDITABLE)
+        binding.lvKoordinat.adapter = listAdapter(this, R.layout.list_item, daftar)
+        binding.lvKoordinat.isClickable = true
+        binding.lvKoordinat.setOnItemClickListener { parent, view, position, id ->
+            val alertDialog = AlertDialog.Builder(this)
+            val view = layoutInflater.inflate(R.layout.input_dialog, null)
+            alertDialog.setView(view)
+            val alertDialogView = alertDialog.show()
+            binding2.RealmId.setText(dataNamaKoordinat[position]+1)
+            binding2.ETinputNama.setText(dataNamaKoordinat[position],TextView.BufferType.EDITABLE)
+            binding2.ETinputCo.setText(dataKoordinat[position],TextView.BufferType.EDITABLE)
+
+            //Delete
+            binding2.delete.setOnClickListener{
+                alertDialogView.dismiss()
+            }
+
+            //Copy
+            binding2.copy.setOnClickListener{
+                copyCoordinate()
+                alertDialogView.dismiss()
+            }
+
+            //Update
+            binding2.update.setOnClickListener{
+                alertDialogView.dismiss()
+            }
         }
 
         //Save
-        binding.save
+        binding.save.setOnClickListener{
+            realm.beginTransaction()
+            var count:Int = 0
+            realm.where(Model::class.java).findAll().let {
+                for (i in it){
+                    count++
+                }
+            }
+            try {
+                val user = realm.createObject(Model::class.java)
+                user.setId(count+1)
+                user.setNama(binding.ETinputNama.text.toString())
+                user.setkoordinat(binding.ETinputCo.text.toString())
 
-        //Delete
-        binding.delete
+                binding.ETinputNama.setText("")
+                binding.ETinputCo.setText("")
 
-        //Copy
-        binding.copy.setOnClickListener{
-            copyCoordinate()
+                realm.commitTransaction()
+            } catch(e: RealmException){
+                Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
+            }
         }
 
         //Help
@@ -67,20 +110,22 @@ class MainActivity : AppCompatActivity() {
         binding.help.setOnClickListener{
             startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://support.google.com/maps/answer/18539?hl=id&co=GENIE.Platform%3DAndroid")))
         }
+    }
 
-        //RealmRealm.init(this)
-//        val config = RealmConfiguration.Builder()
-//           .name("Scan.db")
-//           .schemaVersion(1)
-//           .build()
-//        Realm.setDefaultConfiguration(config)
+    private fun initRealm() {
+        realm = Realm.getDefaultInstance()
     }
 
     private fun copyCoordinate() {
         val clipboardManager = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-        val clipCoordinat = ClipData.newPlainText("Coordinate", binding.ETinputCo.text)
+        val clipCoordinat = ClipData.newPlainText("Coordinate", binding2.ETinputCo.text)
         clipboardManager.setPrimaryClip(clipCoordinat)
 
         Toast.makeText(this, "Koordinat telah dicopy", Toast.LENGTH_SHORT).show()
+    }
+
+    fun getAllUser(){
+        realm.where(Model::class.java).findAll().let {
+        }
     }
 }
